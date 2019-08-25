5421e986d48b5ea248b4da87b27b81e02a9c4c87
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-08-25 12:48:31 -0700

    Customized dmenu appearance
    Updated font to fira mono
    Added gruvbox colors

--- config.h
+++ config.h
@@ -1,23 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Fira Mono:style=Regular:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*					fg			bg			*/
+	// gruvbox colors
+	[SchemeNorm] =	{	"#928374",	"#282828" },
+	[SchemeSel] =	{	"#ebdbb2",	"#282828" },
+	// no idea what this does
+	[SchemeOut] =	{	"#000000",	"#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+

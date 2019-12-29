6f9d7b10d12cad9a6ed53dac80464061b84dcad9
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-12-29 00:52:51 -0700

    Updated colors

--- config.h
+++ config.h
@@ -1,3 +1,5 @@
+// vim: set foldmethod=marker:
+
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
@@ -12,14 +14,20 @@ static const char *fonts[] = {
 /* -p  option; prompt to the left of input field */
 static const char *prompt = NULL;
 
+// gruvbox colors
+
+static const char col_foreground[] = "#ebdbb2";
+static const char col_background[] = "#282828";
+static const char col_cursorColor[] = "#ebdbb2";
+
+static const char col_color6[] = "#689d6a";
+static const char col_color14[] = "#8ec07c";
+
 static const char *colors[SchemeLast][2] = {
-	/*					fg			bg			*/
-	// gruvbox colors
-	[SchemeNorm] =	{	"#a89984",	"#282828" },
-	[SchemeSel] =	{	"#282828",	"#a89984" },
-	// this is a multiple selection thing
-	// it's the color of already selected items
-	[SchemeOut] =	{	"#282828",	"#928374" },
+	/* [name]    = { fg       , bg        } */
+	[SchemeNorm] = { col_foreground,  col_background },
+	[SchemeSel]  = { col_background, col_color14 },
+	[SchemeOut]  = { col_background, col_color6 },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
